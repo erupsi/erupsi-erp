4a78e78b144a22754d2b8e6265b3e49881eaaae4
neiveIsAfraid <muhammad.iqbal.ferhan@gmail.com>
2025-07-16 21:08:36 +0800

    change: adding endpoints on auth-service as a base

--- src/backend/auth-service/src/index.js
+++ src/backend/auth-service/src/index.js
@@ -6,6 +6,22 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/auth/login", (req, res) => {
+    res.send("this is a login endpoint");
+});
+
+app.get("/auth/refresh-token", (req, res) => {
+    res.send("This is a refresh token endpoint");
+});
+
+app.get("/auth/change-password", (req, res) => {
+    res.send("This is a change-password endpoint");
+});
+
+app.get("/auth/register", (req, res) => {
+    res.send("This is a register endpoint");
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
