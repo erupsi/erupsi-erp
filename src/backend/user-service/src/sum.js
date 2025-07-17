2558965b48615d05bf2d71dc8817a18b7b27b14c
andinaufal120 <111062395+andinaufal120@users.noreply.github.com>
2025-07-17 07:10:25 +0000

    style: lint sum.js and sum.test.js

--- src/backend/user-service/src/sum.js
+++ src/backend/user-service/src/sum.js
@@ -1,5 +1,11 @@
+/**
+ * Returns the sum of two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
 function sum(a, b) {
-  return a + b;
+    return a + b;
 }
 
 module.exports = sum;
