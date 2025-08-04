c328d205f02695b2ee53270bfb230174274f1fa3
perhanjay <11241056@student.itk.ac.id>
2025-08-04 15:06:42 +0800

    feature: adding csrf authorization to login and csrf-token endpoint

---
+++ src/backend/auth-service/src/middlewares/csrfProtect.js
@@ -0,0 +1,28 @@
+const csrf = require('csurf');
+
+const csrfProtection = csrf({
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Set secure: true di produksi
+        sameSite: 'Lax',
+    }
+});
+
+const csrfHandler = async (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+};
+
+const handlerErrorCsrf = async (err, req, res, next) => {
+    // Periksa apakah error berasal dari csurf
+    if (err.code === 'EBADCSRFTOKEN') {
+        // Kirim respons 403 Forbidden dengan pesan kustom
+        return res.status(403).json({
+            message: 'Token CSRF tidak valid. Permintaan diblokir.'
+        });
+    }
+    console.error(err)
+    // Jika error bukan dari CSRF, teruskan ke error handler berikutnya (jika ada)
+    next(err);
+};
+
+module.exports = {csrfProtection, csrfHandler, handlerErrorCsrf}
