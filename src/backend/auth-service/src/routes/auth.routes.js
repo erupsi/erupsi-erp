12adfc82fc9561bf06b7fb2cc1edaf4a0326fef8
perhanjay <11241056@student.itk.ac.id>
2025-08-04 10:24:28 +0800

    change: change closing bracket placement

--- src/backend/auth-service/src/routes/auth.routes.js
+++ src/backend/auth-service/src/routes/auth.routes.js
@@ -10,29 +10,41 @@ const logoutHandler = require('../controllers/logoutHandler');
 const validateRegisterReq = require('../validationator/validateRegisterReq');
 const employeeChangePassword = require('../controllers/employeeChangePassword');
 const validateEmployeeChangePassword = require('../validationator/validateEmployeeChangePassword');
+const validateAdminResetPassword = require('../validationator/validateAdminResetPassword');
+const adminResetPassword = require('../controllers/adminResetPassword');
 
 router.post("/register", 
   authenticateServiceReq({useRole:true}), 
   validateRegisterReq, 
-  registerEmployee);
+  registerEmployee
+);
 
 router.post("/login",
   // authenticateServiceReq(),
   validateLoginReq(),
-  loginEmployee)
+  loginEmployee
+);
 
 router.post("/refresh-token", 
   authenticateServiceReq(),
-  refreshTokenRenewal)
+  refreshTokenRenewal
+);
 
 router.post("/logout",
   authenticateServiceReq(),
   logoutHandler
-)
+);
 
 router.post("/change-password",
   authenticateServiceReq(),
   validateEmployeeChangePassword(),
   employeeChangePassword
-)
+);
+
+router.post("/reset-password",
+  authenticateServiceReq({useRole: true}),
+  validateAdminResetPassword(),
+  adminResetPassword
+);
+
 module.exports = router
