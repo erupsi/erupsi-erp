77515488a6c0a84b8566bc9ba190c29a97daaa24
perhanjay <11241056@student.itk.ac.id>
2025-08-09 18:33:08 +0800

    change: change of name to standarize .env vars

--- src/backend/user-service/src/services/db.js
+++ src/backend/user-service/src/services/db.js
@@ -2,11 +2,11 @@ const {Pool} = require("pg");
 require("dotenv").config({path: __dirname + "/../../../../../.env"});
 
 const pool = new Pool({
-    user: process.env.DB_USER_SERVICE,
-    host: process.env.DB_HOST_SERVICE,
-    database: process.env.DB_NAME_SERVICE,
-    password: process.env.DB_PASS_SERVICE,
-    port: process.env.DB_PORT_SERVICE,
+    user: process.env.DB_USER_URM_SERVICE,
+    host: process.env.DB_HOST_URM_SERVICE,
+    database: process.env.DB_NAME_URM_SERVICE,
+    password: process.env.DB_PASS_URM_SERVICE,
+    port: process.env.DB_PORT_URM_SERVICE,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
